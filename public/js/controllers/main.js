e7829edd463ae67acc07afccac1302dd44e2f966
Daniel Fynaardt <daniel.fynaardt@gmail.com>
2014-08-23 17:56:25 -0700

    Complete part 3 of the tutorial 	- client side organization and structure

---
+++ public/js/controllers/main.js
@@ -0,0 +1,38 @@
+// js/controllers/main.js
+
+angular.module('todoController', [])
+  
+  .controller('mainController', function($scope, $http, Todos) {
+    $scope.formData = {};
+
+    // GET =====================================================================
+    // when landing on the page, get all todos and show them
+    // use the service to get all the todos
+    Todos.get()
+      .success(function(data) {
+        $scope.todos = data;
+      });
+
+    // CREATE ==================================================================
+    // when submitting the add form, send the text to the node API
+    $scope.createTodo = function() {
+
+      if( !$.isEmptyObject($scope.formData) ) {
+        Todos.create($scope.formData)
+
+          .success(function(data) {
+            $scope.formData = {}; // clear the form so our user is ready to input another
+            $scope.todos = data;
+          });
+      }
+    };
+
+    // DELETE ==================================================================
+    // delete a todo after checking it
+    $scope.deleteTodo = function(id) {
+      Todos.delete(id)
+        .success(function(data) {
+          $scope.todos = data;
+        });
+    };
+  });
